55a433fc1bd9df59363dc41c61e69f4086f6b12a
Ryan William Tierney <ryan.tierney92@gmail.com>
2019-09-07 15:04:18 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,6 @@
 def square_array(array)
-  while number = [1, 2, 3]
+  while number = [1,2,3]
   square_array(numbers)
-  new_numbers = [9, 10, 16, 25]
+  new_numbers = [9,10,16,25]
   square_array(new_numbers)
 end
